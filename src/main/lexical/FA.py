0ce2d3fe2310753ef83d9d26f35c283d08e2befb
shan <m18351889600@163.com>
2022-10-14 17:35:25 +0800

    update fa

--- src/main/lexical/FA.py
+++ src/main/lexical/FA.py
@@ -1,3 +1,6 @@
+import string
+
+
 class FA:
     def __init__(self, k: list[int], letters: list[str], f: [[str]], s: int, z: int):
         self.k = k  # 状态集
@@ -5,3 +8,32 @@ class FA:
         self.f = f  # 转换函数
         self.s = s  # 唯一初态
         self.z = z  # 终态集
+
+    def __str__(self):
+        return 'k:\t' + str(self.k) + '\n' + \
+               'letters:\t' + str(self.letters) + '\n' + \
+               'f:\t' + str(self.f) + '\n' + \
+               's:\t' + str(self.s) + '\n' + \
+               'z:\t' + str(self.z) + '\n'
+
+
+def get_fa_c_minus() -> FA:
+    return FA(k=None,
+              letters=get_fa_c_minus_letters(),
+              f=None,
+              s=None,
+              z=None)
+
+
+def get_fa_c_minus_letters() -> list[str]:
+    letters = ['+', '-', '*', '/', '%', '=', '<', '>', '!', '&', '|',  # 运算符
+               '(', ')', '{', '}', ',', ';', '_', ]  # 界符
+
+    for letter in string.ascii_lowercase:
+        letters.append(letter)
+    for letter in string.ascii_uppercase:
+        letters.append(letter)
+    for i in range(10):
+        letters.append(str(i))
+
+    return letters

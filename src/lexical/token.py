69bc5fecfddaaa707261de60f887180060ed6a97
shan <zwh.20@qq.com>
2022-10-20 21:54:29 +0800

    token第二种写法

--- src/lexical/token.py
+++ src/lexical/token.py
@@ -1,22 +1,46 @@
-from enum import unique, IntEnum
+from enum import Enum, unique
+
+LexicalUnitKeyword = {
+    '_int': 'int',
+    '_void': 'void',
+    '_return': 'return',
+    '_const': 'const',
+    '_main': 'main',
+}
+LexicalUnitDelimiter = {
+    'l_par': '(',
+    'r_par': ')',
+    'l_brace': '{',
+    'r_brace': '}',
+    'comma': ',',
+    'semicolon': ';'
+}
+
+LexicalUnitOp = {
+    '_commonOp': '+-*/%',
+    '_compareOp': '< > <= >= == !=',
+    '_boolOp': '&& ||',
+    '_assignOp': '='
+}
+
+LexicalUnitOther = {
+    '_IDN': 'idn',  # 标识符: 变量名、函数名
+    'C': 'c',  # 整形常数
+    'fp': 'fp',  # 浮点型常数
+}
 
 
-@unique  # val值应该不相同
-class LexicalUnit(IntEnum):  # 词法单元
-    V = 1,  # 变量
-    C = 2,  # 整数常数
-    INT = 3,
-    VOID = 4,
-    RETURN = 5,
-    CONST = 6,
-    MAIN = 7,
-    LPAR = 8,  # (
-    RPAR = 9,  # )
-    LBRACE = 10,  # {
-    RBRACE = 11,  # }
-    COMMA = 11,  # ,
-    SEMICOLON = 12,  # ;
-    OP = 13  # 运算符
+@unique
+class LexicalUnit(Enum):  # 词法单元
+    def __init__(self):
+        for unit in LexicalUnitKeyword.items():
+            LexicalUnit[unit[0]] = unit[1]
+        for unit in LexicalUnitDelimiter.items():
+            LexicalUnit[unit[0]] = unit[1]
+        for unit in LexicalUnitOp.items():
+            LexicalUnit[unit[0]] = unit[1]
+        for unit in LexicalUnitOther.items():
+            LexicalUnit[unit[0]] = unit[1]
 
 
 class Token:

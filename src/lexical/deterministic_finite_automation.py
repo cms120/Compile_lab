177fab61ec122016fb37ba015edd35477750ff01
FangjingE <614182859@qq.com>
2022-10-21 19:18:35 +0800

    update

--- src/lexical/deterministic_finite_automation.py
+++ src/lexical/deterministic_finite_automation.py
@@ -1,8 +1,8 @@
-from sqlalchemy import false
+import random
 
-from src.lexical.finite_automation import FA
-from src.lexical.rules import Rules
-from src.lexical.state import State
+
+from finite_automation import FA
+
 
 
 class DFA(FA):
@@ -13,57 +13,70 @@ class DFA(FA):
             fa_2_dfa(fa))
 
 
-def epsilon_closure_Move(I: list, letter: str, fa: FA):  # 将集合I转为J=move(I,letter)，再转为Ia=epsilon_closure{j}
-    J = []
-    I_letter = []
-    for state in I:  # move动作
-        key = (state, str)
-        if (fa.f[key] != None):
-            J.append(fa.f[key])
-
-    for state in J:  # 得到Ia
-        for state_2 in state.epsilonTransitions:
-            if not (I_letter.__contains__(state_2)):
-                I_letter.append[state_2]
-
-    return I_letter
-    pass
-
 
 def fa_2_dfa(fa: FA) -> DFA:  # NFA确定化
+    defaultNewStateL=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    
+    dfa_k = []
+    dfa_f = []
+    dfa_letters = fa[1]
+    dfa_s = ''
+    dfa_z = []
+    
+    Listdict = []
+    char = random.choice(defaultNewStateL)
+    defaultNewStateL.remove(char)
+    Listdict.append([[fa[3]],char])
+    dfa_s = char
+    if len(fa[0])==1:
+     dfa_z.append[char]
 
-    ListOfNewStateList = []  # 存储某状态经过epsilon_closure_Move后得到的状态列表
-    dict1 = {}  # 字典dict1存储状态集——>新状态的映射
-    state = State(False)
-    Allstate = State.get_state_all  # get_state_all是不是改成静态方法好点？
+    ListOfStateList = [list[0] for list in Listdict]
+    for StateList in ListOfStateList:
+          for letter in fa[1]:
+           for State in StateList:            
+               for tuple in fa[2]:
+                if tuple[0] == (State,letter):
+                 for Statee in tuple[1]:
+                    StateList2 = []
+                    if not StateList2.__contains__(Statee):
+                     StateList2.append(Statee)
+               if not ListOfStateList.__contains__(StateList2):   
+                char = random.choice(defaultNewStateL)
+                defaultNewStateL.remove(char)
+                Listdict.append([StateList2,char])
+                ListOfStateList=[list[0] for list in Listdict]
+                for list4 in Listdict:
+                    if list4[0]==StateList:
+                        dfa_f.append((list4[1],letter),[char])
 
-    for state1 in Allstate:  # 获得fa的开始状态
-        if (fa.s == state1.get_flag):
-            S0 = state1
+                for str in fa[4]:
+                 if StateList2.__contains__(str):
+                  dfa_z.append(char)
+                  break
+                
+                 
+                 
 
-    for letter in fa.letters:
-        ListOfNewStateList.append(epsilon_closure_Move(S0.epsilonTransitions, letter,
-                                                       fa))  # 开始状态的Epsilon闭包通过letter达到的状态组成一个新状态集，存进ListOfNewStateList
-        state2 = State(false)
-        dict1[epsilon_closure_Move(S0.epsilonTransitions, letter, fa)] = state2  # 把初始状态集映射到一个新的状态
-        # S0.transitions[letter]=state2
+    
 
-    for StateList in ListOfNewStateList:  # 遍历ListOfNewStateList中的状态集，经每个letter通过epsilon_closure_Move得到新的状态集再加进ListOfNewStateList再遍历，重复这个过程直到不产生新的状态集为止
-        for letter in fa.letters:
-            NewStateList = epsilon_closure_Move(StateList, letter, fa)
-            if not (ListOfNewStateList.__contains__(NewStateList)):
-                ListOfNewStateList.append(NewStateList)
-                state3 = state(false)
-                dict1[NewStateList] = state3  # 同样将得到的状态集NewStateList映射到一个新状态
-                dict1[StateList].transitions[letter] = dict1[
-                    NewStateList]  # 设置原状态的transitions映射：StateList映射到的原状态——letter——>NewStateList映射到的新状态
+    for value in dict.values():
+     dfa_k.append(value)
+    
+     
+    
+    dfa = [
+        dfa_k,
+        dfa_letters,
+        dfa_f,
+        dfa_s,
+        dfa_z
+          ]
+    
 
-    rules = Rules(dict1.get(next(iter(dict1))),
-                  dict1.get(list(dict1.keys())[-1]))  # 取字典中第一个值（状态）和最后一个值作为rule的start和end（python3中字典是按序存储的）
-
-    Dfa = Rules.init_by_rules(rules)  # 根据规则构造FA，得到的是DFA
-
-    return Dfa
+             
+                        
+    return dfa
     pass
 
 
@@ -75,3 +88,5 @@ def dfa_minimize(dfa) -> DFA:  # DFA最小化
     # 3.对两个集合根据是否可再分继续划分集合，直到不可再划分为止
 
     pass
+
+

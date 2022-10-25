72866dbc3b064334e3153c59d8916bd781835879
shan <zwh.20@qq.com>
2022-10-25 18:05:35 +0800

    update graph

--- src/lexical/graph.py
+++ src/lexical/graph.py
@@ -4,22 +4,20 @@ from src.lexical.deterministic_finite_automation import DFA
 from src.lexical.finite_automation import FA
 
 
-class Graph:
-    dfa_name = 1
+def graph_fa_print(fa: FA):  # 画NFA的图像
 
-    @staticmethod
-    def graph_fa_print(fa: FA):  # 画NFA的图像
+    g = Digraph(comment='graph_fa')
+    for f in fa.f.items():  # 遍历转换函数
+        for val in f[1]:
+            g.edge(f[0][0], val, f[0][1])
 
-        g = Digraph('G', filename='DFA' + str(Graph.dfa_name) + '.gv', format='png')
-        for f in fa.f.items():
-            for val in f[1]:
-                g.edge(f[0][0], val, f[0][1])
+    g.node(fa.s, color='res')  # 开始节点红色
+    for z in fa.z:
+        g.node(z, shape='doublecircle')  # 结束节点双层
 
-        g.node(fa.s, color='res')  # 开始节点红色
-        for z in fa.z:
-            g.node(z, shape='doublecircle')  # 结束节点双层
+    g.render('DFA.gv', view=True)
 
-        Graph.dfa_name += 1
 
-        g.view()
-
+def graph_dfa_print(dfa: DFA):
+    fa = FA(dfa.k, dfa.letters, dfa.f, dfa.s, dfa.z)
+    graph_fa_print(fa)
--- src/test/lexical/test_graph.py
+++ src/test/lexical/test_graph.py
@@ -1,21 +1,25 @@
 import unittest
 
-from src.lexical.finite_automation import FA
-from src.lexical.graph import Graph
+from src.lexical.finite_automation import FA, get_fa_c_minus
+from src.lexical.graph import graph_fa_print
 from src.lexical.regex import Regex
 from src.lexical.rules import Rules
 from src.test.lexical.test_case import re_s
 
 
 class GraphTest(unittest.TestCase):
+    def test_graph_fa_c_minus(self):
+        self.assertTrue(True)
+        graph_fa_print(get_fa_c_minus())
+
     def test_graph_fa(self):
         self.assertTrue(True)
-        for re in re_s:
-            print(re)
-            re_postfix = Regex.get_re_postfix(re)
-            print(re_postfix)
-            rules = Rules.init_by_re_postfix(re_postfix)
-            # print(rules)
-            fa = FA.init_by_rules(rules)
-            print(fa)
-            Graph.graph_fa_print(fa)
+
+        print(re_s[0])
+        re_postfix = Regex.get_re_postfix(re_s[0])
+        print(re_postfix)
+        rules = Rules.init_by_re_postfix(re_postfix)
+        # print(rules)
+        fa = FA.init_by_rules(rules)
+        print(fa)
+        graph_fa_print(fa)

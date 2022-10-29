687ad793aaa6506f61388803a8224930ba8ef22e
shan <zwh.20@qq.com>
2022-10-29 12:03:16 +0800

    add c_minus_words

---
+++ src/c_mimus.py
@@ -0,0 +1,5 @@
+c_minus_keyword = ['int', 'void', 'const', 'main', 'return']
+c_minus_unary_op = ['+', '-', '*', '/', '%', '=', '<', '>']
+c_minus_binary_op = ['==', '<=', '>=', '!=', '&&', '||']
+c_minus_op = c_minus_unary_op + c_minus_binary_op
+c_minus_se = [':|', '(', ')', '{', '}', ';', ',']
